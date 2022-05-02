f9720ac4ce49d61d7d7f6c78dfa77aa7ce165158
Eduardo da Silva <eduardo.socio14@gmail.com>
2022-05-02 17:47:18 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require ("express");
+const app = express();
+
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/html/index.html")
+});
+
+app.get("/sobre", function(req,res){
+    res.sendFile(__dirname + "/html/sobre.html")
+});
+
+app.get("/blog", function(req,res){
+    res.send("Bem-vindo ao meu blog jovem padawan!")
+});
+
+app.get("/ola/:nome/:cargo/:cor", function(req,res){
+    res.send("<h1>Olá "+req.params.nome+"<h1>"+"<h2>Seu cargo é "+req.params.cargo+"<h2>"+"<h3>Sua cor favorita é "+req.params.cor+"<h3>")
+});
+
+app.listen(8081, function(){
+    console.log("Servidor rodando na url http://localhost:8081 jovem padawan!")
+});
